e397fcf7a205dc8434d426916088cbc4ac3ded83
Yesung-Han <930805hys@gmail.com>
2023-01-02 21:01:58 +0900

    [INIT] :sparkles: scroll 기능 실험

--- index.js
+++ index.js
@@ -8,32 +8,125 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __await = (this && this.__await) || function (v) { return this instanceof __await ? (this.v = v, this) : new __await(v); }
+var __asyncGenerator = (this && this.__asyncGenerator) || function (thisArg, _arguments, generator) {
+    if (!Symbol.asyncIterator) throw new TypeError("Symbol.asyncIterator is not defined.");
+    var g = generator.apply(thisArg, _arguments || []), i, q = [];
+    return i = {}, verb("next"), verb("throw"), verb("return"), i[Symbol.asyncIterator] = function () { return this; }, i;
+    function verb(n) { if (g[n]) i[n] = function (v) { return new Promise(function (a, b) { q.push([n, v, a, b]) > 1 || resume(n, v); }); }; }
+    function resume(n, v) { try { step(g[n](v)); } catch (e) { settle(q[0][3], e); } }
+    function step(r) { r.value instanceof __await ? Promise.resolve(r.value.v).then(fulfill, reject) : settle(q[0][2], r); }
+    function fulfill(value) { resume("next", value); }
+    function reject(value) { resume("throw", value); }
+    function settle(f, v) { if (f(v), q.shift(), q.length) resume(q[0][0], q[0][1]); }
+};
+var __asyncValues = (this && this.__asyncValues) || function (o) {
+    if (!Symbol.asyncIterator) throw new TypeError("Symbol.asyncIterator is not defined.");
+    var m = o[Symbol.asyncIterator], i;
+    return m ? m.call(o) : (o = typeof __values === "function" ? __values(o) : o[Symbol.iterator](), i = {}, verb("next"), verb("throw"), verb("return"), i[Symbol.asyncIterator] = function () { return this; }, i);
+    function verb(n) { i[n] = o[n] && function (v) { return new Promise(function (resolve, reject) { v = o[n](v), settle(resolve, reject, v.done, v.value); }); }; }
+    function settle(resolve, reject, d, v) { Promise.resolve(v).then(function(v) { resolve({ value: v, done: d }); }, reject); }
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const elasticsearch_1 = require("@elastic/elasticsearch");
 require("dotenv/config");
+const INDEX = process.env.ES_INDEX_PATTERN || '';
+console.log(INDEX);
 const client = new elasticsearch_1.Client({
-    cloud: { id: process.env.ES_CLOUD_NODE || '' },
+    node: process.env.ES_CLOUD_NODE || '',
     auth: {
         username: process.env.ES_CLOUD_USERNAME || '',
         password: process.env.ES_CLOUD_PASSWORD || ''
     }
 });
-const runQuery = (index) => __awaiter(void 0, void 0, void 0, function* () {
+const runSearch = (index) => __awaiter(void 0, void 0, void 0, function* () {
+    var _a, _b;
     try {
         // new definitions
         const response = yield client.search({
-            index: 'test',
+            index,
+            size: 10000,
             body: {
                 query: {
-                    match: { foo: '123' }
+                    match_all: {}
                 }
             }
         });
-        console.log(response.body);
+        const documents = ((_b = (_a = response === null || response === void 0 ? void 0 : response.body) === null || _a === void 0 ? void 0 : _a.hits) === null || _b === void 0 ? void 0 : _b.hits) || [];
+        return documents;
     }
     catch (error) {
         // (error as AxiosError).message
         console.log(error);
+        throw error;
     }
 });
-runQuery('').catch(console.log);
+function scrollSearch(params) {
+    return __asyncGenerator(this, arguments, function* scrollSearch_1() {
+        let response = yield __await(client.search(params));
+        while (true) {
+            const sourceHits = response.body.hits.hits;
+            if (sourceHits.length === 0) {
+                break;
+            }
+            for (const hit of sourceHits) {
+                yield yield __await(hit);
+            }
+            if (!response.body._scroll_id) {
+                break;
+            }
+            console.log(response);
+            response = yield __await(client.scroll({
+                scroll_id: response.body._scroll_id,
+                scroll: params.scroll
+            }));
+        }
+    });
+}
+const runScroll = (index) => __awaiter(void 0, void 0, void 0, function* () {
+    var _c, e_1, _d, _e;
+    try {
+        // new definitions
+        const params = {
+            index,
+            scroll: '30s',
+            size: 10000,
+            body: {
+                query: {
+                    match_all: {}
+                }
+            }
+        };
+        let results = [];
+        try {
+            for (var _f = true, _g = __asyncValues(scrollSearch(params)), _h; _h = yield _g.next(), _c = _h.done, !_c;) {
+                _e = _h.value;
+                _f = false;
+                try {
+                    const hit = _e;
+                    results.push(hit);
+                }
+                finally {
+                    _f = true;
+                }
+            }
+        }
+        catch (e_1_1) { e_1 = { error: e_1_1 }; }
+        finally {
+            try {
+                if (!_f && !_c && (_d = _g.return)) yield _d.call(_g);
+            }
+            finally { if (e_1) throw e_1.error; }
+        }
+        return results;
+    }
+    catch (error) {
+        // (error as AxiosError).message
+        throw error;
+    }
+});
+const main = () => __awaiter(void 0, void 0, void 0, function* () {
+    const res = yield runScroll(INDEX);
+    console.log([res[0]], res.length);
+});
+main();
--- index.ts
+++ index.ts
@@ -4,31 +4,109 @@ import {
 } from '@elastic/elasticsearch'
 import 'dotenv/config'
 
+const INDEX: string = process.env.ES_INDEX_PATTERN || '';
+console.log(INDEX)
+
+type ESDocument = {
+  _index: string
+  _type: string
+  _id: string
+  _score: number,
+  _source: object
+}
+type QueryResults = ESDocument[] | [];
+type SearchParam = {
+  index: string,
+  size?: number,
+  body: object | string
+  scroll?: string
+}
+
 const client: Client = new Client({
-  cloud: { id: process.env.ES_CLOUD_NODE || '' },
+  node: process.env.ES_CLOUD_NODE || '',
   auth: { 
     username: process.env.ES_CLOUD_USERNAME || '',
     password: process.env.ES_CLOUD_PASSWORD || ''
   }
 })
 
-const runQuery = async (index: string) => {
+const runSearch = async (index: string): Promise<QueryResults> => {
   try {
     // new definitions
     const response: ApiResponse = await client.search({
-      index: 'test',
+      index,
+      size: 10000, // max 10000
       body: {
-      query: {
-          match: { foo: '123' }
-        }
+        query: {
+            match_all: {}
+          }
       }
     })
-
-    console.log(response.body);
+    const documents: QueryResults = response?.body?.hits?.hits || [];
+    return documents;
   } catch (error: unknown) {
     // (error as AxiosError).message
     console.log(error);
+    throw error;
   }
 }
 
-runQuery(process.env.ES_INDEX_PATTERN || '').catch(console.log);
+async function * scrollSearch (params: SearchParam) {
+  let response: ApiResponse = await client.search(params)
+
+  while (true) {
+    const sourceHits: QueryResults = response.body.hits.hits
+
+    if (sourceHits.length === 0) {
+      break
+    }
+
+    for (const hit of sourceHits) {
+      yield hit
+    }
+
+    if (!response.body._scroll_id) {
+      break
+    }
+
+    console.log(response);
+
+    response = await client.scroll({
+      scroll_id: response.body._scroll_id,
+      scroll: params.scroll
+    })
+  }
+}
+
+const runScroll = async (index: string): Promise<QueryResults> => {
+  try {
+    // new definitions
+    const params = {
+      index,
+      scroll: '30s', // 
+      size: 10000, // max 10000
+      body: {
+        query: {
+          match_all: {}
+        }
+      }
+    }
+    
+    let results: ESDocument[] = [];
+    for await (const hit of scrollSearch(params)) {
+      results.push(hit);
+    }
+
+    return results;
+  } catch (error: unknown) {
+    // (error as AxiosError).message
+    throw error;
+  }
+}
+
+const main = async () => {
+  const res: QueryResults = await runScroll(INDEX);
+  console.log([res[0]], res.length);
+}
+
+main();
